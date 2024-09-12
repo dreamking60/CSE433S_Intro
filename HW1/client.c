19dca5f8ec1f585833a92ad604f52e07d7a4f645
dreamking60 <dreamlyboyczh@foxmail.com>
2024-09-12 13:05:54 -0500

    fix error

--- HW1/client.c
+++ HW1/client.c
@@ -6,35 +6,43 @@
 
 int main() {
     // Declare Variables
+    struct sockaddr_in server_addr;
+
     char server_ip[16] = "192.168.92.132";
     int server_port = 10888;
     char server_message[1024];
     char client_message[1024];
 
+    ssize_t varread;
+
     // Create socket:
-    int sock;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
     // Send connection request to server, be sure to set port and IP the same as server-side
-    if(connect(sock, server_ip, server_port) < 0) {
+    if(connect(sock, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) {
         perror("Connection failed with error!");
         return 1;
     }
 
     // Get input from the user:
     printf("Enter message sent to the server: ");
-    fgets(client_message);
+    fgets(client_message, sizeof(client_message), stdin);
 
     // Send the message to server:
     send(sock, client_message, strlen(client_message), 0);
 
     // Receive the server's response:
-    recv(sock, server_message, 1024, 0);
+    varread = recv(sock, server_message, 1024, 0);
 
     printf("Server's response: %s\n",server_message);
 
     // Close the socket:
-    close(sock);
-    
+    //close(sock);
+
     return 0;
 
 }
--- HW1/server.c
+++ HW1/server.c
@@ -3,54 +3,73 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <netinet/in.h>
 
 int main() {
     // Declare variables
-	char server_ip[16] = "192.168.92.132";
-    int server_port = 10888;
+	ssize_t varread;
     char server_message[1024];
     char client_message[1024];
 
+    struct sockaddr_in server_addr;
+    char server_ip[16]= "192.168.92.132";
+    int server_port = 10888;
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
 
     // Create socket
-  	int sock;
     int client_sock;
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock == 0) {
-        perror("Socket failed with error!");
+  	int sever_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sever_sock < 0) {
+        perror("Socket creation failed with error!");
         return 1;
     }
+    if(setsockopt(sever_sock, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
+        perror("Socket option failed with error!");
+        return 1;
+    }
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    
+
     // Bind to the set port and IP
-    printf("Done with binding with IP: %s, Port: %d\n", server_ip, server_port);
-    if(bind(sock, server_ip, server_port) < 0) {
-        perror("Bind failed with error!");
+    if(bind(sever_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Binding failed with error!");
         return 1;
     }
+    printf("Done with binding with IP: %s, Port: %d\n", server_ip, server_port);
+
     // Listen for clients:
-    if(socket_listen(sock, 3) < 0) {
-        perror("Listen failed with error!");
+    if(listen(sever_sock, 3) < 0) {
+        perror("Listening failed with error!");
         return 1;
     }
 
     // Accept an incoming connection
-    if(socket_accept(sock, client_sock, client_ip, client_port) < 0) {
-        perror("Accept failed with error!");
+    if((client_sock = accept(sever_sock, (struct sockaddr*)&client_addr, &client_addr_len)) < 0) {
+        perror("Accepting failed with error!");
         return 1;
     }
+    char * client_ip = inet_ntoa(client_addr.sin_addr);
+    int client_port = ntohs(client_addr.sin_port);
     printf("Client connected at IP: %s and port: %i\n", client_ip, client_port);
 
     // Receive client's message
-    client_message = socket_recv(client_sock, 1024);
+    varread = recv(client_sock, client_message, 1024, 0);
     printf("Msg from client: %s\n", client_message);
 
 
     // Respond to client
     strcpy(server_message, "##Hello, Bob! This is Alice.##");
-    socket_send(client_sock, server_message, strlen(server_message));
+    send(client_sock, server_message, strlen(server_message), 0);
 
     // Close the socket
-    close(client_sock);
-    close(sock);
+    //close(client_sock);
+    //close(sever_sock);
+    
 
     return 0;
 }
