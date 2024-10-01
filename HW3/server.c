8cc8af0a10b1137e879c890f7cb0154f47cb4f6a
dreamking60 <dreamlyboyczh@foxmail.com>
2024-10-01 15:19:01 -0500

    Base64 debug

--- HW3/server.c
+++ HW3/server.c
@@ -27,52 +27,13 @@ void handleErrors(void)
 }
 
 // Base64 decode
-int base64_decode(const unsigned char *input, int length, unsigned char *output) {
-    BIO *bio, *b64;
-    int decoded_len;
-
-    b64 = BIO_new(BIO_f_base64());
-    bio = BIO_new_mem_buf(input, length);
-    bio = BIO_push(b64, bio);
-
-    // Disable newlines
-    BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
-
-    // Decode the input data
-    decoded_len = BIO_read(bio, output, length);
-    if (decoded_len < 0) {
-        BIO_free_all(bio);
-        return -1; // Decoding failed
-    }
-
-    // Clean up
-    BIO_free_all(bio);
-
-    return decoded_len;
+void base64_decode(const unsigned char *input, int length, unsigned char *output) {
+    return EVP_DecodeBlock(output, input, length);
 }
 
 // Base64 encode
-int base64_encode(const unsigned char *input, int length, unsigned char *output) {
-    BIO *bio, *b64;
-    BUF_MEM *buffer_ptr;
-
-    b64 = BIO_new(BIO_f_base64());
-    bio = BIO_new(BIO_s_mem());
-    bio = BIO_push(b64, bio);
-
-    // Write the input data to the BIO
-    BIO_write(bio, input, length);
-    BIO_flush(bio);
-    BIO_get_mem_ptr(bio, &buffer_ptr);
-
-    // Copy the encoded data to the output buffer
-    memcpy(output, buffer_ptr->data, buffer_ptr->length);
-    output[buffer_ptr->length] = '\0'; // Null-terminate the output
-
-    // Clean up
-    BIO_free_all(bio);
-
-    return buffer_ptr->length;
+void base64_encode(const unsigned char *input, int length, unsigned char *output) {
+    return EVP_EncodeBlock(output, input, length);
 }
 
 int block_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext)
