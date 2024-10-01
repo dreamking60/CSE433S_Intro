1b5def6dc9b5e5afda9885d62f140784999927c0
dreamking60 <dreamlyboyczh@foxmail.com>
2024-10-01 14:21:11 -0500

    debug base64 decode

--- HW3/server.c
+++ HW3/server.c
@@ -235,28 +235,6 @@ int main() {
     printf("Base64 Encoded Key: %s\n", base64_encoded_key);
     printf("Base64 Encoded IV: %s\n", base64_encoded_iv);
 
-    // Decode the key and iv in new variables
-    unsigned char decoded_key[AES_KEY_LENGTH];
-    unsigned char decoded_iv[AES_BLOCK_SIZE];
-
-    // Base64 decode the key and iv
-    base64_decode(base64_encoded_key, base64_encoded_key_len, decoded_key);
-    base64_decode(base64_encoded_iv, base64_encoded_iv_len, decoded_iv);
-
-    // Print the decoded key and iv
-    printf("Decoded Key: ");
-    for (int i = 0; i < AES_KEY_LENGTH; i++) {
-        printf("%02x", decoded_key[i]);
-    }
-    printf("\n");
-
-    printf("Decoded IV: ");
-    for (int i = 0; i < AES_BLOCK_SIZE; i++) {
-        printf("%02x", decoded_iv[i]);
-    }
-    printf("\n");
-
-
     // Send the key to the client
     send(client_sock, base64_encoded_key, base64_encoded_key_len, 0);
 
