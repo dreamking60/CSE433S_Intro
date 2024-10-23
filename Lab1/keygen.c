c9c74ea2ef7e3cd7e60a6bba2abe12cd52819d94
dreamking60 <dreamlyboyczh@foxmail.com>
2024-10-22 23:13:02 -0500

    key

--- Lab1/keygen.c
+++ Lab1/keygen.c
@@ -6,7 +6,9 @@ void main()
 {
     
     char key[KEYSIZE];
-    for(time_t i = 1524017329; i <= 1524024529; i++) {
+    time_t start = 1524017329;
+    time_t end = 1524024529;
+    for(time_t i = start; i <= end; i++) {
         srand(i);
         for (int j = 0; j< KEYSIZE; j++){
             key[j] = rand()%256;
