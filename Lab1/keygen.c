4ec3cd16854da0a8ec8598ff14875595eb39f012
dreamking60 <dreamlyboyczh@foxmail.com>
2024-10-22 23:13:37 -0500

    key

--- Lab1/keygen.c
+++ Lab1/keygen.c
@@ -4,13 +4,13 @@
 #define KEYSIZE 16
 void main()
 {
-    
+    int j;
     char key[KEYSIZE];
     time_t start = 1524017329;
     time_t end = 1524024529;
     for(time_t i = start; i <= end; i++) {
         srand(i);
-        for (int j = 0; j< KEYSIZE; j++){
+        for (j = 0; j< KEYSIZE; j++){
             key[j] = rand()%256;
             printf("%.2x", (unsigned char)key[j]);
         }
